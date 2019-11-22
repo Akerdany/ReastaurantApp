c856029679ec448a771fa82e602a483b17968f38
Ahmed ElKerdany <akerdany@gmail.com>
2019-11-23 01:55:58 +0200

    Sign Up Layout added

--- app/src/main/java/com/example/reastaurantapp/SignUpActivity.java
+++ app/src/main/java/com/example/reastaurantapp/SignUpActivity.java
@@ -3,6 +3,7 @@ package com.example.reastaurantapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+    }
+
+//    TODO: Same work as the main
+
+    public void signUp(View view) {
     }
 }
