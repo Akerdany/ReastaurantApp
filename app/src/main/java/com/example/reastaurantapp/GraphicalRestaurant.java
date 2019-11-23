593c51d0f4f3ce425c98e992fd7b03682b445a72
shehababoelrejal <shobymoh@gmail.com>
2019-11-23 19:41:08 +0200

    Layout of Graphical interface with pop up done.

--- app/src/main/java/com/example/reastaurantapp/GraphicalRestaurant.java
+++ app/src/main/java/com/example/reastaurantapp/GraphicalRestaurant.java
@@ -2,13 +2,42 @@ package com.example.reastaurantapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class GraphicalRestaurant extends AppCompatActivity {
-
+public class GraphicalRestaurant extends AppCompatActivity
+{
+    Dialog myDialog;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graphical_restaurant);
+        myDialog = new Dialog(this);
+
+    }
+
+    public void ShowPopup(View view)
+    {
+        TextView txtclose;
+        Button btnFollow;
+        myDialog.setContentView(R.layout.activity_popupmenu);
+        txtclose =(TextView) myDialog.findViewById(R.id.txtclose);
+        txtclose.setText("X");
+        btnFollow = (Button) myDialog.findViewById(R.id.btnfollow);
+        txtclose.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                myDialog.dismiss();
+            }
+        });
+        myDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        myDialog.show();
     }
 }
