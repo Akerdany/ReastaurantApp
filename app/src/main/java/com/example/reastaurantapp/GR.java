4d289454015f0028b709942f75efa039c9daccdf
Ahmed ElKerdany <akerdany@gmail.com>
2019-12-02 19:23:06 +0200

    Revert "Pop menu done"
    This reverts commit 774fcfc0a0f7cf54f4148555be17894f7372f276.

--- app/src/main/java/com/example/reastaurantapp/GR.java
+++ app/src/main/java/com/example/reastaurantapp/GR.java
@@ -16,8 +16,7 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class GR extends Fragment
-{
+public class GR extends Fragment {
     Dialog myDialog;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -27,5 +26,23 @@ public class GR extends Fragment
 
     }
 
+    public void ShowPopup(View view)
+    {
+        TextView txtclose;
+        Button btnFollow;
+        myDialog.setContentView(R.layout.fragment_popupmenufrag);
+        txtclose =(TextView) myDialog.findViewById(R.id.txtclose);
+        txtclose.setText("X");
+        btnFollow = (Button) myDialog.findViewById(R.id.btnfollow);
+        txtclose.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                myDialog.dismiss();
+            }
+        });
+        myDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        myDialog.show();
+    }
 
 }
