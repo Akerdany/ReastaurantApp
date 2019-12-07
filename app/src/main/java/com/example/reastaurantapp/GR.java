f5bafb430f647a6e694409b71eb5755d96f9fb62
shehababoelrejal <shobymoh@gmail.com>
2019-12-07 15:28:11 +0200

    Push Reservations To Database Done.

--- app/src/main/java/com/example/reastaurantapp/GR.java
+++ app/src/main/java/com/example/reastaurantapp/GR.java
@@ -1,31 +1,19 @@
 package com.example.reastaurantapp;
 
-import android.app.Dialog;
-import android.content.Context;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
-import android.net.Uri;
-import android.os.Bundle;
 
+import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 public class GR extends Fragment
 {
-    Dialog myDialog;
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_gr, container, false);
-
     }
-
-
 }
--- app/src/main/java/com/example/reastaurantapp/HomePageActivity.java
+++ app/src/main/java/com/example/reastaurantapp/HomePageActivity.java
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.app.TimePickerDialog;
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
@@ -25,43 +26,38 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.Calendar;
 
-public class HomePageActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
-
-    int Day, Month, Year, Hour, Minute;
-    int dayFinal, monthFinal, yearFinal, hourFinal, minuteFinal;
-
-    Dialog myDialog;
-
+public class HomePageActivity extends AppCompatActivity
+{
     BottomNavigationView bottomNav;
     private BottomNavigationView.OnNavigationItemSelectedListener navigationListener =
-            new BottomNavigationView.OnNavigationItemSelectedListener() {
-                @Override
-                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                    Fragment selectedFragment = null;
+    new BottomNavigationView.OnNavigationItemSelectedListener()
+    {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item)
+        {
+            Fragment selectedFragment = null;
+            //TODO: Add the other menu cases buttons
+            switch (item.getItemId())
+            {
+                case R.id.nav_home_icon:
+                    selectedFragment = new GR();
+                    break;
+                case R.id.nav_more_icon:
+                    selectedFragment = new MorePage();
+                    break;
 
-                    //TODO: Add the other menu cases buttons
-                    switch (item.getItemId()) {
-                        case R.id.nav_home_icon:
-                            selectedFragment = new GR();
-                            break;
-                        case R.id.nav_more_icon:
-                            selectedFragment = new MorePage();
-                            break;
-
-                         default:
-                             selectedFragment = new GR();
-                             break;
-                    }
-
-                    getSupportFragmentManager().beginTransaction().replace(R.id.homepage_fragement,
-                            selectedFragment).commit();
-
-                    return true;
-                }
-            };
+                 default:
+                     selectedFragment = new GR();
+                     break;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.homepage_fragement, selectedFragment).commit();
+            return true;
+        }
+    };
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage_client);
 
@@ -72,63 +68,9 @@ public class HomePageActivity extends AppCompatActivity implements DatePickerDia
                 new GR()).commit();
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-    public void ShowPopup(View v) {
-        myDialog = new Dialog(this);
-        myDialog.create();
-        TextView txtclose;
-        ImageView image = findViewById(R.id.viewImage);
-        Button btnFollow;
-        myDialog.setContentView(R.layout.activity_popupmenu);
-        switch (v.getId()) {
-            case R.id.tablefour1:
-                image.setImageResource(R.drawable.table4);
-                break;
-            case R.id.tabletwo1:
-                image.setImageResource(R.drawable.table2);
-                break;
-        }
-        txtclose = (TextView) myDialog.findViewById(R.id.txtclose);
-        txtclose.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                myDialog.dismiss();
-            }
-        });
-        myDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-        myDialog.show();
-    }
-
-    public void chooseDate(View view) {
-        Calendar c = Calendar.getInstance();
-        Year = c.get(Calendar.YEAR);
-        Month = c.get(Calendar.MONTH);
-        Day = c.get(Calendar.DAY_OF_MONTH);
-        DatePickerDialog datediag = new DatePickerDialog(HomePageActivity.this, HomePageActivity.this, Year, Month, Day);
-        datediag.show();
-        myDialog.dismiss();
-    }
-
-    @Override
-    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-        yearFinal = year;
-        monthFinal = month + 1;
-        dayFinal = dayOfMonth;
-        Calendar c = Calendar.getInstance();
-        Hour = c.get(Calendar.HOUR_OF_DAY);
-        Minute = c.get(Calendar.MINUTE);
-        TimePickerDialog timediag = new TimePickerDialog(HomePageActivity.this, HomePageActivity.this, Hour, Minute, DateFormat.is24HourFormat(this));
-        timediag.show();
-    }
-
-    @Override
-    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-        hourFinal = hourOfDay;
-        minuteFinal = minute;
-    }
-
-    @Override
-    public void onPointerCaptureChanged(boolean hasCapture) {
-
+    public void SwitchToGR(View view)
+    {
+        Intent intent = new Intent(this, GraphicalRestaurant.class);
+        startActivity(intent);
     }
 }
