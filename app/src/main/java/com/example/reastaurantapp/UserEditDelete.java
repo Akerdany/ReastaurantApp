75721e271c7949c2f33d62afd6c76c12591438fc
shehababoelrejal <shobymoh@gmail.com>
2019-12-26 21:19:57 +0200

    Admin panel

---
+++ app/src/main/java/com/example/reastaurantapp/UserEditDelete.java
@@ -0,0 +1,14 @@
+package com.example.reastaurantapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserEditDelete extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_edit_delete);
+    }
+}
